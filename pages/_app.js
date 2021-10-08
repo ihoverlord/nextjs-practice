650d75152e1b5773cd9e21a0648118a6c1b1b2bc
ihoverlord <himakarreddy@hotmail.com>
2021-10-09 00:38:50 +0530

    chakra ui added. firestore added. user context added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import { ProvideAuth } from "@/lib/auth";
+import { ThemeProvider, CSSReset } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import theme from "@/styles/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <ProvideAuth>
+        <Component {...pageProps} />
+      </ProvideAuth>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    body: `Inter`
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 700
+  }
+});
+
+export default theme;
